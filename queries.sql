18e4744c87c64321af61a3a4d13733e32a2086c1
SlavaIoffe <ioffeslav@gmail.com>
2026-05-27 20:27:06 +0300

    Delete queries.sql

--- queries.sql
+++
@@ -1,6 +0,0 @@
-
---запрос считает общее количество покупателей из таблицы customers. И выводит данные в колонку customers_count.
-Select
-    Count(customers.customer_id) As customers_count
-From
-    customers;
