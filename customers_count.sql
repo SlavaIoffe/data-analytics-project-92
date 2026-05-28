f1ffb3471275e8147f4cd95b6f02e6e9b48380bc
SlavaIoffe <ioffeslav@gmail.com>
2026-05-28 12:17:06 +0300

    Add files via upload

---
+++ customers_count.sql
@@ -0,0 +1,7 @@
+﻿--запрос считает общее количество покупателей из таблицы customers. И выводит данные в колонку customers_count.
+--customers_count.csv
+
+    SELECT
+      Count(customers.customer_id) AS customers_count
+FROM
+    customers
