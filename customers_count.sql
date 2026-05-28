2c8c203cf5cc37585cecb6e7f57eafb02aa798e6
SlavaIoffe <ioffeslav@gmail.com>
2026-05-28 12:17:45 +0300

    Delete customers_count.sql

--- customers_count.sql
+++
@@ -1,7 +0,0 @@
-﻿--запрос считает общее количество покупателей из таблицы customers. И выводит данные в колонку customers_count.
---customers_count.csv
-
-    SELECT
-      Count(customers.customer_id) AS customers_count
-FROM
-    customers
